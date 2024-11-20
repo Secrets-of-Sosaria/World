3325dd4cc310f053864f2d55fecd3b64e52a478f
EstelMatiazi <estelmatiazi@gmail.com>
2024-11-20 17:15:12 -0300

    adds user configuration to determine if items should go past skill cap or not

--- Data/Scripts/System/Misc/AOS.cs
+++ Data/Scripts/System/Misc/AOS.cs
@@ -886,7 +886,7 @@ namespace Server
 					m_Mods = new List<SkillMod>();
 
 				SkillMod sk = new DefaultSkillMod( skill, true, bonus );
-				sk.ObeyCap =  true;
+				sk.ObeyCap =  !MySettings.S_itemsOvercapSkills;
 				m.AddSkillMod( sk );
 				m_Mods.Add( sk );
 			}
