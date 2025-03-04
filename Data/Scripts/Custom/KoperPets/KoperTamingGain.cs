cd7d97bc1e1574812abfceb88325ff4706a02459
Sirata89 <nicholas.kent.t@gmail.com>
2025-03-04 14:42:18 -0700

    Update KoperTamingGain.cs
    Update comments

--- Data/Scripts/Custom/KoperPets/KoperTamingGain.cs
+++ Data/Scripts/Custom/KoperPets/KoperTamingGain.cs
@@ -1,3 +1,29 @@
+/*
+
+Created by Kopernicus
+
+KoperPets
+
+A pet system modification for the Secrets of Sosaria UO Shard.
+
+This script adds a small chance to gain taming skill when your pet fights, decreasing in amount and frequency as you increase in taming skill. Also included is a battle cry for your pets, they will occasionally remark on the battle.
+Installation
+
+WARNING: If you have modified your basecreature.cs script, do not replace it with this one. You will need to manually add the call for taming gain. Search my basecreature.cs script for KOPER.
+
+(This has been save file safe to add and remove for me, as it serializes no data. BUT, back up your save just for that warm fuzzy)
+
+    Be running current version of SoS
+
+    Place KoperPets folder in Data/Scripts/Custom
+
+    Backup your original Data/Scripts/Mobiles/Base/BaseCreature.cs
+
+    Place BaseCreature.cs from this repositiory in Data/Scripts/Mobiles/Base, replacing the original
+
+
+*/
+
 using System;
 using System.Collections.Generic;
 using Server;
