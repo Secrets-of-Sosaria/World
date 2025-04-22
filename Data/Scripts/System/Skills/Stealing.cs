64e0f317d1697d0d3e65831aee9fe08a812d9eed
EstelMatiazi <estelmatiazi@gmail.com>
2025-04-22 19:57:59 -0300

    adds the contrand system to the game

--- Data/Scripts/System/Skills/Stealing.cs
+++ Data/Scripts/System/Skills/Stealing.cs
@@ -12,6 +12,7 @@ using Server.Spells.Necromancy;
 using Server.Spells;
 using Server.Spells.Ninjitsu;
 using Server.Misc;
+using Server.Systems;
 
 namespace Server.SkillHandlers
 {
@@ -412,6 +413,11 @@ namespace Server.SkillHandlers
 					from.AddToBackpack( stolen );
 
 					StolenItem.Add( stolen, m_Thief, root as Mobile );
+					Mobile m = target as Mobile;
+					if (m != null)
+					{
+					    ContrabandSystem.TryGiveContraband(from, m);
+					}
 				}
 
 				if ( caught )
